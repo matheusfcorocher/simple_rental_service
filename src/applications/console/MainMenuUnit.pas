a26cc09de07ceef7e8e8f198399e83c6056b9d99
matheusfcorocher <56937223+matheusfcorocher@users.noreply.github.com>
2024-12-10 13:18:05 -0300

    refactor: replace CleanConsole of Menus for CleanConsole of ConsoleUtilsUnit

--- src/applications/console/MainMenuUnit.pas
+++ src/applications/console/MainMenuUnit.pas
@@ -2,39 +2,13 @@ unit MainMenuUnit;
 
 interface
 
-uses Winapi.Windows, System.SysUtils;
+uses ConsoleUtilsUnit;
 
 procedure Menu;
-
-procedure CleanConsole;
 
 implementation
 
 uses ClientMenuUnit, VehiclesMenuUnit, RentalMenuUnit;
-
-procedure CleanConsole;
-var
-  stdout: THandle;
-  csbi: TConsoleScreenBufferInfo;
-  ConsoleSize: DWORD;
-  NumWritten: DWORD;
-  Origin: TCoord;
-begin
-  // retrieve the handle of console output
-  stdout := GetStdHandle(STD_OUTPUT_HANDLE);
-  Win32Check(stdout <> INVALID_HANDLE_VALUE);
-  // retrieve information about the console buffer
-  Win32Check(GetConsoleScreenBufferInfo(stdout, csbi));
-  // calculate the number of characters in buffer
-  ConsoleSize := csbi.dwSize.X * csbi.dwSize.Y;
-  Origin.X := 0;
-  Origin.Y := 0;
-  //fill the buffer with spaces and the original attributes
-  Win32Check(FillConsoleOutputCharacter(stdout, ' ', ConsoleSize, Origin, NumWritten));
-  Win32Check(FillConsoleOutputAttribute(stdout, csbi.wAttributes, ConsoleSize, Origin, NumWritten));
-  //set cursor in position top-left corner
-  Win32Check(SetConsoleCursorPosition(stdout, Origin));
-end;
 
 procedure Menu;
 var
--- src/applications/console/VehiclesMenuUnit.pas
+++ src/applications/console/VehiclesMenuUnit.pas
@@ -2,9 +2,7 @@ unit VehiclesMenuUnit;
 
 interface
 
-uses Winapi.Windows, System.SysUtils;
-
-procedure CleanConsole;
+uses ConsoleUtilsUnit;
 
 // procedures related to Vehicle
 procedure VehiclesMenu;
@@ -16,30 +14,6 @@ procedure DeleteVehicle;
 implementation
 
 uses MainMenuUnit;
-
-procedure CleanConsole;
-var
-  stdout: THandle;
-  csbi: TConsoleScreenBufferInfo;
-  ConsoleSize: DWORD;
-  NumWritten: DWORD;
-  Origin: TCoord;
-begin
-  // retrieve the handle of console output
-  stdout := GetStdHandle(STD_OUTPUT_HANDLE);
-  Win32Check(stdout <> INVALID_HANDLE_VALUE);
-  // retrieve information about the console buffer
-  Win32Check(GetConsoleScreenBufferInfo(stdout, csbi));
-  // calculate the number of characters in buffer
-  ConsoleSize := csbi.dwSize.X * csbi.dwSize.Y;
-  Origin.X := 0;
-  Origin.Y := 0;
-  //fill the buffer with spaces and the original attributes
-  Win32Check(FillConsoleOutputCharacter(stdout, ' ', ConsoleSize, Origin, NumWritten));
-  Win32Check(FillConsoleOutputAttribute(stdout, csbi.wAttributes, ConsoleSize, Origin, NumWritten));
-  //set cursor in position top-left corner
-  Win32Check(SetConsoleCursorPosition(stdout, Origin));
-end;
 
 procedure VehiclesMenu;
 var

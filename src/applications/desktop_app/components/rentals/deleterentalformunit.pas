fb8279bba37280a3799898a44ac9149ed4cfd4ac
matheusfcorocher <56937223+matheusfcorocher@users.noreply.github.com>
2025-01-16 17:49:50 -0300

    feat: add delete rental form

--- src/applications/desktop_app/components/rentals/deleterentalformunit.pas
+++ src/applications/desktop_app/components/rentals/deleterentalformunit.pas
@@ -5,10 +5,19 @@ unit DeleteRentalFormUnit;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, fpjson, jsonparser, GlobalVariablesUnit, StringGridUtilsUnit;
 
 type
+
+  { TDeleteRentalForm }
+
   TDeleteRentalForm = class(TForm)
+    DeleteButton: TButton;
+    IdLabel: TLabel;
+    IdTextField: TEdit;
+    TextMessageError: TLabel;
+    TitleLabel: TLabel;
+    procedure DeleteButtonClick(Sender: TObject);
   private
 
   public
@@ -20,7 +29,42 @@ var
 
 implementation
 
+uses MainFormUnit;
+
 {$R *.lfm}
+
+{ TDeleteRentalForm }
+
+procedure TDeleteRentalForm.DeleteButtonClick(Sender: TObject);
+var
+  RentalJSON: TJSONObject;
+  Response: TJSONObject;
+
+  RowIndex: integer;
+begin
+    RentalJSON := TJSONObject.Create;
+    RentalJSON.Add('id', IdTextField.Text);
+
+    Response := RentalService.GetRentalController().Delete(RentalJSON);
+
+    if Response.Get('success', False) = true then
+    begin
+      // getting data field
+      TextMessageError.Visible := False;
+
+      // find row in string grid
+      RowIndex := FindRowWithValueInStringGrid(MainForm.StringGrid3, RentalJSON.Get('id', ''), 1);
+      // Search in column 1
+
+      // Delete row
+      MainForm.StringGrid3.DeleteRow(RowIndex);
+    end
+    else
+    begin
+      TextMessageError.Caption := Response.Get('message', '');
+      TextMessageError.Visible := True;
+    end;
+end;
 
 end.
 
